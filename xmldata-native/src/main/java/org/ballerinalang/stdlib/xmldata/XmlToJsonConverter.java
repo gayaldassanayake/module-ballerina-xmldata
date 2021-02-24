8a190ce384352b24e12bd6db4338c6088b4cdae5
Kalaiyarasi <ashakalai31@gmail.com>
2021-02-24 22:44:09 +0530

    Change license header year

--- xmldata-native/src/main/java/org/ballerinalang/stdlib/xmldata/XmlToJsonConverter.java
+++ xmldata-native/src/main/java/org/ballerinalang/stdlib/xmldata/XmlToJsonConverter.java
@@ -1,5 +1,5 @@
 /*
- *   Copyright (c) 2019, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+ *   Copyright (c) 2021, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
  *
  *  WSO2 Inc. licenses this file to you under the Apache License,
  *  Version 2.0 (the "License"); you may not use this file except
@@ -50,7 +50,7 @@ import static io.ballerina.runtime.api.utils.StringUtils.fromString;
 /**
  * Converts a XML to the corresponding JSON representation.
  *
- * @since 1.0
+ * @since 2.0.0
  */
 public class XmlToJsonConverter {
 
